cdc8ac37892dd552b9ef6a10ed7166b94753fee9
Marc <marc@aurai.com>
2023-01-26 09:24:41 +0000

    DBT course - Models

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,34 +1,19 @@
 -- config is used to create a table instead of a view, automatically is removed from view on snowflake if dbt run was already executed.
 -- And it will create the table with the same name under tables
 -- only run model: dim_customers --> dbt run --select dim_customers
-{{ 
-    config (
-    materialized='table'
-    )
-}}
 
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
-
+    
+    select * from {{ref ('stg_customers') }}
+    
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    select * from {{ref ('stg_orders') }}
 
 ),
+
 
 customer_orders as (
 
